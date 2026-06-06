e1c70d7d22f5d9897690fee8f088814380d1ea6a
Sahil Khan <arya89nkhan@gmail.com>
2026-06-06 23:59:48 +0530

    stack

---
+++ Day05/StackArrayDemo.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+class Stack {
+    int[] arr;
+    int top;
+    int size;
+
+    Stack(int size) {
+        this.size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int data) {
+        if (top == size - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+
+        arr[++top] = data;
+        System.out.println(data + " pushed");
+    }
+
+    void pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return;
+        }
+
+        System.out.println(arr[top--] + " popped");
+    }
+
+    void peek() {
+        if (top == -1) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+
+        System.out.println("Top Element: " + arr[top]);
+    }
+
+    void display() {
+        if (top == -1) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+
+        System.out.println("Stack Elements:");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class StackArrayDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter stack size: ");
+        int n = sc.nextInt();
+
+        Stack s = new Stack(n);
+
+        while (true) {
+            System.out.println("\n1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Display");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter element: ");
+                    int data = sc.nextInt();
+                    s.push(data);
+                    break;
+
+                case 2:
+                    s.pop();
+                    break;
+
+                case 3:
+                    s.peek();
+                    break;
+
+                case 4:
+                    s.display();
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
