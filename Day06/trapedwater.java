5802219123ca1ad8124697aa051bd3f91d73d241
Sahil Khan <arya89nkhan@gmail.com>
2026-06-08 23:56:38 +0530

    traped

--- Day06/trapedwater.java
+++ Day06/trapedwater.java
@@ -1,7 +1,26 @@
 import java.util.*;
 public class trapedwater {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] h = new int[n];
+        for(int i=0; i<n; i++){
+            h[i] = sc.nextInt();
+        }
+        int [] left = new int[n];
+        int [] right = new int[n];
+        left[0] = h[0];
+        right[n-1] = h[n-1];
+        for(int i=1; i<n; i++){
+            left[i] = Math.max(h[i], left[i-1]);
+        }
+        for(int i = n-2; i>=0; i++){
+            right[i] =  Math.max(h[i],right[i+1]);
+        }
+
+        for(int i =0; i<n; i++){
+            int wl = new 
+        }
     }
     
 }
