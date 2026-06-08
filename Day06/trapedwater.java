3351be30a6dba8a2d4a361714c4ed629e6bc2c7d
Sahil Khan <arya89nkhan@gmail.com>
2026-06-08 23:59:50 +0530

    traped water

--- Day06/trapedwater.java
+++ Day06/trapedwater.java
@@ -14,13 +14,17 @@ public class trapedwater {
         for(int i=1; i<n; i++){
             left[i] = Math.max(h[i], left[i-1]);
         }
-        for(int i = n-2; i>=0; i++){
+        for(int i = n-2; i>=0; i--){
             right[i] =  Math.max(h[i],right[i+1]);
         }
+        int traped = 0;
 
         for(int i =0; i<n; i++){
-            int wl = new 
+            int wl = Math.min(left[i], right[i]);
+            traped += wl-h[i];
         }
+        System.out.println("traped water " +traped);
+        sc.close();
     }
     
 }
