000abe4f8b0b53399a6e80835a68650c15f0ecae
Sahil Khan <arya89nkhan@gmail.com>
2026-06-11 22:59:01 +0530

    update

--- Day07/Stringuti.java
+++ Day07/Stringuti.java
@@ -4,6 +4,7 @@ public class Stringuti {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
 
         String str = sc.nextLine();
 
